25d770bebb7cc7c46e29c179c72eb5c464abd50d
Veeramani <vveerama@cis.neustar.com>
2017-02-17 13:10:40 +0530

    2nd Version - Takes Input and modifies Label accordingly

--- scripts/simpleGUI_tkinter.py
+++ scripts/simpleGUI_tkinter.py
@@ -12,27 +12,31 @@ class simpleapp_tk(Tkinter.Tk):
     def initialize(self):
         self.grid()
 
-        self.entry = Tkinter.Entry(self)
+        self.entryVariable = Tkinter.StringVar()
+        self.entry = Tkinter.Entry(self, textvariable=self.entryVariable)
         self.entry.grid(column=0, row=0, stick='EW')
         self.entry.bind("<Return>", self.OnPressEnter)
+        self.entryVariable.set(u"Enter Text Here.")
         
         button = Tkinter.Button(self,text=u"Click me !", command=self.OnButtonClick)
         button.grid(column=1, row=0)
         
-        label = Tkinter.Label(self,
+        self.labelVariable = Tkinter.StringVar()
+        label = Tkinter.Label(self, textvariable=self.labelVariable,
                               anchor="w",
                               fg="white",
                               bg="blue")
         label.grid(column=0, row=1, columnspan=2, sticky='EW')
+        self.labelVariable.set(u"Hello !")
         
         self.grid_columnconfigure(0, weight=1)
         self.resizable(True, False)
     
     def OnButtonClick(self):
-        print "You Clicked the Button"
+        self.labelVariable.set( self.entryVariable.get() + " (You Clicked the button !) ")
     
     def OnPressEnter(self, event):
-        print "You Pressed Enter"
+        self.labelVariable.set( self.entryVariable.get() + " (You Pressed Enter !) ")
         
 
 if __name__ == "__main__":
